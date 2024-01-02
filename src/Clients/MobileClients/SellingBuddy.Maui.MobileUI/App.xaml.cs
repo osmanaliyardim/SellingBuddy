75974bff28fd5502257edf2c164fa5918e19ce54
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-01-02 12:47:56 +0300

    add project layers

---
+++ src/Clients/MobileClients/SellingBuddy.Maui.MobileUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SellingBuddy.Maui.MobileUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Clients/MobileClients/SellingBuddy.Maui.MobileUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SellingBuddy.Maui.MobileUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
