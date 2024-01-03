9cc209411d66c0cb84d0a3425629a4c8d767f7bc
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-01-03 13:41:13 +0300

    feat: add azure service bus

---
+++ src/BuildingBlocks/EventBus/SellingBuddy.EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,18 @@
+﻿using SellingBuddy.EventBus.AzureServiceBus;
+using SellingBuddy.EventBus.Base;
+using SellingBuddy.EventBus.Base.Abstraction;
+using SellingBuddy.EventBus.RabbitMQ;
+
+namespace SellingBuddy.EventBus.Factory;
+
+public static class EventBusFactory
+{
+    public static IEventBus Create(EventBusConfig eventBusConfig, IServiceProvider serviceProvider)
+    {
+        return eventBusConfig.EventBusType switch
+        {
+            EventBusType.AzureServiceBus => new EventBusServiceBus(serviceProvider, eventBusConfig),
+            _ => new EventBusRabbitMQ(serviceProvider, eventBusConfig)
+        };
+    }
+}
