3cce4e830106cbe0ab04c9805235f3deaf0a9354
Osman Ali Yardım <osmanaliyardim@gmail.com>
2024-01-06 13:01:08 +0300

    feat: add simple identity service

---
+++ src/Services/IdentityService/SellingBuddy.IdentityService.Api/Application/Services/IIdentityService.cs
@@ -0,0 +1,8 @@
+﻿using SellingBuddy.IdentityService.Api.Application.Models;
+
+namespace SellingBuddy.IdentityService.Api.Application.Services;
+
+public interface IIdentityService
+{
+    Task<LoginResponseModel> Login(LoginRequestModel model);
+}
